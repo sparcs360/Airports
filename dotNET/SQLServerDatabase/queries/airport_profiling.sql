f4482229a4bfe9958b726d090f5ba6536569d4a0
Lee Newfeld (Home) <lee.newfeld@gmail.com>
2016-09-05 17:27:05 +0100

    Rename top level folder to dotNet (from C#)

---
+++ dotNET/SQLServerDatabase/queries/airport_profiling.sql
@@ -0,0 +1,57 @@
+﻿/*
+
+SELECT	*
+FROM	Airport
+WHERE	ident = 'EGLL'
+
+SELECT	COUNT(*)
+FROM	Airport
+WHERE	ident IS NULL
+
+SELECT	COUNT(*)
+FROM	Airport
+WHERE	LEN(iata_code) = 0
+
+SELECT	COUNT(*)
+FROM	Airport
+WHERE	LEN(local_code) = 0
+
+SELECT	ident,
+		COUNT(*)
+FROM	Airport
+GROUP BY
+		ident
+HAVING	COUNT(*) > 1
+ORDER BY
+		COUNT(*) DESC
+
+SELECT	iso_region
+,		municipality
+,		COUNT(*)
+FROM	Airport
+GROUP BY
+		iso_region
+,		municipality
+--HAVING	COUNT(*) > 1
+ORDER BY
+		COUNT(*) DESC
+
+SELECT	iso_region
+,		COUNT(*)
+FROM	Airport
+GROUP BY
+		iso_region
+--HAVING	COUNT(*) > 1
+ORDER BY
+		COUNT(*) DESC
+
+---
+
+SELECT	A.iata_code
+,		A.gps_code
+FROM	Airport A
+JOIN	Country C ON (C.code = A.iso_country)
+WHERE	A.ident = 'EGLL'
+GO
+
+*/
---
+++ dotNET/SQLServerDatabase/queries/country_profiling.sql
@@ -0,0 +1,23 @@
+﻿/*
+SELECT	*
+FROM	Country
+
+SELECT	code,
+		COUNT(*)
+FROM	Country
+GROUP BY
+		code
+HAVING	COUNT(*) > 1
+ORDER BY
+		COUNT(*) DESC
+
+SELECT	code,
+		COUNT(*)
+FROM	Country
+GROUP BY
+		code
+HAVING	COUNT(*) > 1
+ORDER BY
+		COUNT(*) DESC
+
+*/
