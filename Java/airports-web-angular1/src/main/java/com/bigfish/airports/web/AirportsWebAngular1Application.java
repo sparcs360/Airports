61e5f44cc11969f5658d25860eb84862e3a87aff
Lee Newfeld (Home) <lee.newfeld@gmail.com>
2016-09-06 11:56:52 +0100

    Initial check-in of airports-web-angular1

---
+++ Java/airports-web-angular1/src/main/java/com/bigfish/airports/web/AirportsWebAngular1Application.java
@@ -0,0 +1,12 @@
+package com.bigfish.airports.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirportsWebAngular1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirportsWebAngular1Application.class, args);
+	}
+}
